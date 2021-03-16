e6fdda0212f9352dd47dce15026ce5ff45ac0298
Anita <haluska99@hotmail.com>
2021-03-16 14:20:04 -0700

    created InterviewListItem, InterviewerList, Appointment:index, Appointment:header, Appointment:Empty components and associated styles

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classnames from  "classnames";
+import "components/Appointment/styles.scss";
+
+
+export default function Appointment() {
+
+  return (
+    <article className="appointment"></article>
+  )
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import classnames from  "classnames";
+import "components/InterviewerListItem.scss";
+
+
+/*Our InterviewerListItem component takes in the following props:
+id:number - the id of the interviewer
+name:string - the name of the interviewer
+avatar:url - a url to an image of the interviewer
+selected:boolean - to determine if an interview is selected or not
+setInterviewer:function - sets the interviewer upon selection 
+*/
+
+
+
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
