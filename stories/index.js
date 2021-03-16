c0abbd2a2f02788122596a0f68171143b3c56bf0
Anita <haluska99@hotmail.com>
2021-03-16 14:50:06 -0700

    created confirm and status components in Appointment and added them to stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,19 +136,27 @@ storiesOf("Button", module)
         ));
 
 
-        storiesOf("Appointment", module)
-        .addParameters({
-            backgrounds: [{ name: "white", value: "#fff", default: true }]
-          })
-          .add("Appointment", () => <Appointment />)
-          .add("Appointment with Time", () => <Appointment time="12pm" />)
-          .add("Header", () => <Header time="12pm" />)
-          .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-          .add("Show", () => (
-            <Show 
-              student="Lydia Miller-Jones"
-              interviewer={interviewer}
-              onEdit={action("onEdit")} 
-              onDelete={action("onDelete")}
-            />
-          ));
+      storiesOf("Appointment", module)
+      .addParameters({
+          backgrounds: [{ name: "white", value: "#fff", default: true }]
+        })
+        .add("Appointment", () => <Appointment />)
+        .add("Appointment with Time", () => <Appointment time="12pm" />)
+        .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+        .add("Show", () => (
+          <Show 
+            student="Lydia Miller-Jones"
+            interviewer={interviewer}
+            onEdit={action("onEdit")} 
+            onDelete={action("onDelete")}
+          />
+        ))
+        .add("Confirm", () => (
+          <Confirm
+            mesasge="Delete the appointment?"
+            onConfirm={action("onConfirm")} 
+            onCancel={action("onCancel")}
+          />
+        ))
+        .add("Status", () => <Status message="Deleting" />)
