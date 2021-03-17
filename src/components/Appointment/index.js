1762e2b54db23783c3a7e13967237140692d56df
Anita <haluska99@hotmail.com>
2021-03-16 17:29:19 -0700

    created Form component with Empty and Edit stories, importing Fragment, created Appointment Booked component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Fragment } from 'react'
 import classnames from  "classnames";
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 
-export default function Appointment() {
+export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interviewer ? <Show /> : < Empty/>}
+    </article>
   )
 }
