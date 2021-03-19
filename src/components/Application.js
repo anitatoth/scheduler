bd8a6d3071d06076db527a8f1d228b57e264549e
Anita <haluska99@hotmail.com>
2021-03-19 10:00:47 -0700

    completed implementing creating appointments and implemented deleting appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,15 +46,28 @@ export default function Application(props) {
       [id]: appointment
     };
 
-
-
-    setState({
-      ...state,
-      appointments
-    });
-
+    return axios.put(`/api/appointments/${id}`, { interview })
+      .then(() => {
+      setState({ ...state, appointments });
+      });
   }
   
+  function cancelInterview(id) {
+    
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+  return axios.delete(`/api/appointments/${id}`)
+    .then(() => setState({ ...state, appointments }))
+
+  };
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -66,6 +79,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,21 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error"
 import useVisualMode from "hooks/useVisualMode";
+
 
 
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const CONFIRM = "CONFIRM";
+const DELETING = "DELETING";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
 
@@ -24,10 +32,15 @@ export default function Appointment(props) {
       interviewer
     };
 
-    transition("SHOW");
-    props.bookInterview(props.id, interview)
+    transition("SAVING")
+    props.bookInterview(props.id, interview) 
+         .then(() => transition("SHOW"))
+  }
 
-  
+  function deleteInterview() {
+    transition("DELETING")
+    props.cancelInterview(props.id)
+         .then(() => transition("EMPTY"))
 
   }
 
@@ -36,10 +49,12 @@ export default function Appointment(props) {
       <Header time={props.time} />
 
       {mode === EMPTY && <Empty onAdd={() => transition("CREATE")} />}
-      {mode === SHOW && (
+      {mode === SHOW && props.interview && (
         <Show
           student={props.interview.student}
+          interview={props.interview}
           interviewer={props.interview.interviewer}
+          onDelete = {() => transition("CONFIRM")}
         />
       )}
       {mode === CREATE && (
@@ -49,8 +64,18 @@ export default function Appointment(props) {
           onCancel={back}
         />
       )}
+      {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message= {"Are you sure you would like to delete?"}
+          onConfirm= {deleteInterview}
+          onCancel= {back}
+          message= {"Are you sure you would like to delete?"}
+        />
+      )}
 
-      
+     
     </article>
   )
 }
